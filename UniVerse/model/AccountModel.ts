e2808559d716aaf90c13cf5ca4395e57b86996c4
William Nwoke <willteey@gmail.com>
2023-04-28 11:52:17 -0700

    Added code for AccountsModel, empty methods

---
+++ UniVerse/model/AccountModel.ts
@@ -0,0 +1,42 @@
+import Mongoose = require("mongoose");
+import {DataAccess} from './../DataAccess';
+import {IAccountModel} from "../interfaces/IAccountModel";
+let mongooseConnection = DataAccess.mongooseConnection;
+let mongooseObj = DataAccess.mongooseInstance;
+
+
+class AccountModel {
+    public schema:any;
+    public model:any;
+
+    public constructor() {
+        this.createSchema();
+        this.createModel();
+    }
+
+    public createSchema(): void {
+        this.schema = new Mongoose.Schema(
+            {
+                Id: Number,
+                username: String,
+                fname: String,
+                lname: String,
+                email: String,
+                department: String
+            }, {collection: 'accounts'} // TODO: review what this line is for
+        )
+    }
+
+    public createModel(): void {
+        this.model = mongooseConnection.model<IAccountModel>("Accounts", this.schema);
+    }
+
+    public createAccount(): any {
+
+    }
+
+    public viewAccount(): any {
+        
+    }
+}
+export {AccountModel};
