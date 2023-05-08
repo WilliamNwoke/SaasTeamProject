6544145ba52576b7e576b1ecbfbd118be86bf2b0
Mustafa Abuthuraya <mostafufu@hotmail.com>
2023-05-07 21:03:24 -0700

    removed inline css, abstracted post-card into its own component

--- UniVerse-app/src/app/app.component.ts
+++ UniVerse-app/src/app/app.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { PostCardComponent } from './post-card/post-card.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [PostCardComponent]
 })
 export class AppComponent {
   title = 'UniVerse-app';
+
+  constructor(private postCardComponent: PostCardComponent) {}
 }
