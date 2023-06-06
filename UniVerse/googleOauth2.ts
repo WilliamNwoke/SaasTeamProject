17e4fb6115b214d5b58057650b540a2a32cbf58f
William Nwoke <willteey@gmail.com>
2023-06-06 16:56:27 -0700

    remove .env

--- UniVerse/googleOauth2.ts
+++ UniVerse/googleOauth2.ts
@@ -1,9 +1,6 @@
-import { config } from 'dotenv';
-config();
-
 class googleOauth2 {
-    static id: string = process.env.ID_FOR_SECRET;
-    static secret:string = process.env.SECRET;
+    static id: string = "389516173031-9dgjki4o5gds4hpgdp5lpgka6911p0v8.apps.googleusercontent.com";
+    static secret:string = "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";
     
     }
     export default googleOauth2;
